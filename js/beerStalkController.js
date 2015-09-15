915a2023a26187823db58c75ad7582204efdadc5
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-09-15 11:50:11 +0100

    added location

--- js/beerStalkController.js
+++ js/beerStalkController.js
@@ -8,8 +8,26 @@ beerStalker.controller('BeerStalkController',['$scope', '$resource', function($s
   //    key: 'key=646f252216306e6d712d7c536a3c2565'}
   //   );
 
-  var searchResource = $resource('https://api.meetup.com/2/open_events.json?and_text=true&:text&:country&:city&:key',
-    {text: "text=free+beer", country: "country=gb", city: "city=London", key: 'key=646f252216306e6d712d7c536a3c2565', callback:'JSON_CALLBACK'}, {get:{method:'JSONP'}})
+/////////////////////////////
+  // var searchResource = $resource('https://api.meetup.com/2/open_events.json?and_text=true&:text&:country&:city&:key',
+  //   {text: "text=free+beer", 
+  //     country: "country=gb", 
+  //     city: variable, 
+  //     key: 'key=646f252216306e6d712d7c536a3c2565', 
+  //     callback:'JSON_CALLBACK'}, 
+  //   {get:{method:'JSONP'}})
+/////////////////////
+
+// self.tasks = [];
+
+//   self.addTask = function() {
+//     self.tasks.push(
+//       { 
+//         "name" : self.taskName,
+//         "completed": false
+//       });
+//     self.taskName = ''
+//   };
 
 // function search(){
 //   searchResource.get().$promise.then(function(response){
@@ -19,10 +37,19 @@ beerStalker.controller('BeerStalkController',['$scope', '$resource', function($s
 // }
 
   $scope.search = function() {
+  var searchResource = $resource('https://api.meetup.com/2/open_events.json?and_text=true&:text&:country&city=:city&:key',
+    {text: "text=free+beer", 
+      country: "country=gb",
+      city: $scope.cityName, 
+      key: 'key=646f252216306e6d712d7c536a3c2565', 
+      callback:'JSON_CALLBACK'}, 
+    {get:{method:'JSONP'}})
+
       searchResource.get().$promise.then(function(response){
           self.searchResult = response.results;
           console.log(self.searchResult[0].name)
       });
+
   }
 
 }]);
