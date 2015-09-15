d637550ea8ca2bb61ca636579ca95f66155ec18f
Toby Clarke <teeohbee@gmail.com>
2015-09-15 10:24:26 +0100

    API promise resolved, still working on displaying results

--- js/beerStalkController.js
+++ js/beerStalkController.js
@@ -1,6 +1,7 @@
-beerStalker.controller('BeerStalkController',['$resource', function($resource) {
+beerStalker.controller('BeerStalkController',['$scope', '$resource', function($scope, $resource) {
+
   var self = this;
-  var searchResource = $resource('https://api.meetup.com/2/open_events.json?:text&:key')
+  // var searchResource = $resource('https://api.meetup.com/2/open_events.json?:text&:key')
 
   // self.searchResult = searchResource.get(
   //   {text: "text=beer",
@@ -8,11 +9,20 @@ beerStalker.controller('BeerStalkController',['$resource', function($resource) {
   //   );
 
   var searchResource = $resource('https://api.meetup.com/2/open_events.json?:text&:key',
-    {text: "text=beer", key: 'key=646f252216306e6d712d7c536a3c2565', callback:'JSON_CALLBACK'}, {get:{method:'JSONP'}})
+    {text: "text=free+beer", key: 'key=646f252216306e6d712d7c536a3c2565', callback:'JSON_CALLBACK'}, {get:{method:'JSONP'}})
 
-  self.searchResult = searchResource.get()
+// function search(){
+//   searchResource.get().$promise.then(function(response){
+//       self.searchResult = response.results;
+//       console.log(self.searchResult[0].name)
+//   });
+// }
 
-  console.log(self.searchResult)
-
+$scope.search = function() {
+    searchResource.get().$promise.then(function(response){
+        self.searchResult = response.results;
+        console.log(self.searchResult[0].name)
+    });
+}
 
 }]);
