4ef0c71d6bc4e9fb5f7e8d19aaccfc77f437f936
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-15 20:46:16 +0100

    error handled when no results

--- js/beerStalkController.js
+++ js/beerStalkController.js
@@ -16,10 +16,20 @@ beerStalker.controller('BeerStalkController', ['$scope', '$resource', function($
         var filteredResults = [];
         for (index = 0; index < response.results.length; index++) {
           var result = response.results[index].description;
+          
+          try {
+            result.indexOf('free beer')
+          } 
+          catch(err) {
+            $('#error_div').fadeIn(500).delay(8000).fadeOut(1000);
+            break;
+          }
+
           if(result.indexOf('free beer') >= 0) {
             filteredResults.push(response.results[index]);
           }
         }
+
         $scope.searchResult = (filteredResults)//filtered by 'free beer';
       });
     };
