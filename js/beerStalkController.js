f47626e01c7fb55d40f3cb65c007042edc87d1e5
Antonio Voce <antonio.v0c3@gmail.com>
2015-09-15 14:45:49 +0100

    results showing on the page

--- js/beerStalkController.js
+++ js/beerStalkController.js
@@ -11,19 +11,10 @@ beerStalker.controller('BeerStalkController',['$scope', '$resource', function($s
   var searchResource = $resource('https://api.meetup.com/2/open_events.json?:text&:key',
     {text: "text=free+beer", key: 'key=646f252216306e6d712d7c536a3c2565', callback:'JSON_CALLBACK'}, {get:{method:'JSONP'}})
 
-// function search(){
-//   searchResource.get().$promise.then(function(response){
-//       self.searchResult = response.results;
-//       console.log(self.searchResult[0].name)
-//   });
-// }
-
   $scope.search = function() {
       searchResource.get().$promise.then(function(response){
-          self.searchResult = response.results;
-          self.searchTry = response.results[0];
-          console.log(self.searchResult[0].visibility)
+          $scope.searchResult = response.results;
+          console.log($scope.searchResult[0].visibility)
       });
-  }
-
+    };
 }]);
