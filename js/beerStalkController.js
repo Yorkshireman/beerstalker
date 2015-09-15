f1d8ec52ba51ddc3862fa728cc2779a9072d84b2
Toby Clarke <teeohbee@gmail.com>
2015-09-15 16:50:34 +0100

    Added dateTime to view

--- js/beerStalkController.js
+++ js/beerStalkController.js
@@ -4,16 +4,20 @@ beerStalker.controller('BeerStalkController', ['$scope', '$resource', function($
 
   $scope.search = function() {
     var searchResource = $resource('https://api.meetup.com/2/open_events.json?and_text=true&:text&:country&:city&:key', {
-        text: "text=free+beer",
-        country: "country=gb",
-        city: "city=" + $scope.cityName,
-        key: 'key=646f252216306e6d712d7c536a3c2565',
-        callback: 'JSON_CALLBACK' },
-        { get: { method: 'JSONP'} });
-        
-      searchResource.get().$promise.then(function(response){
-          $scope.searchResult = response.results;
-          console.log($scope.searchResult[0].visibility)
-      });
-    };
+      text: "text=free+beer",
+      country: "country=gb",
+      city: "city=" + $scope.cityName,
+      key: 'key=646f252216306e6d712d7c536a3c2565',
+      callback: 'JSON_CALLBACK'
+    }, {
+      get: {
+        method: 'JSONP'
+      }
+    });
+
+    searchResource.get().$promise.then(function(response) {
+      $scope.searchResult = response.results;
+      console.log($scope.searchResult[0].time - $scope.searchResult[0].utc_offset)
+    });
+  };
 }]);
