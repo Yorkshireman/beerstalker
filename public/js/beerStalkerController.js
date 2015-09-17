5b1d7b597cc6614b1f655dbf7f9b26026e1218db
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-17 18:25:06 +0100

    autoSearch factory complete. Could maybe DRY up the code because of some repeated code

--- public/js/beerStalkerController.js
+++ public/js/beerStalkerController.js
@@ -2,7 +2,8 @@ beerStalker.factory('ApiCall', function($resource) {
   return {
     customSearch: function(cityName) {
       var meetupUrl = 'https://api.meetup.com/2/open_events.json?and_text=true&:text&:country&:city&:key&:text_format&:order'
-      var searchResource = $resource(
+      
+      var searchForEvents = $resource(
         meetupUrl, 
         {
           text_format: "text_format=plain",
@@ -18,7 +19,7 @@ beerStalker.factory('ApiCall', function($resource) {
 
       //nothing gets returned from this function unless you have the 'return' on line 21
       //the other return further down is only within the scope of that function, not this function.
-      return searchResource.get().$promise.then(function(response){
+      return searchForEvents.get().$promise.then(function(response){
         var filteredResults = [];
         for (index = 0; index < response.results.length; index++) {
           var result = response.results[index].description;
@@ -45,42 +46,33 @@ beerStalker.factory('ApiCall', function($resource) {
           return filteredResults
         }
       });
-    }
+    },
 
-    // autoSearch:
-  }
-});
+    autoSearch: function(lat, lon) {
+      var meetupUrl = 'https://api.meetup.com/2/open_events.json?and_text=true&:text&:lat&:lon&:key&:text_format&:order'
+      
+      var searchForEvents = $resource(
+        meetupUrl, 
+        {
+          text_format: "text_format=plain",
+          text: "text=free+beer",
+          lat: 'lat=' + lat,
+          lon: 'lon=' + lon,
+          key: 'key=646f252216306e6d712d7c536a3c2565',
+          order: "order=distance",
+          callback: 'JSON_CALLBACK' 
+        },
+          { get: { method: 'JSONP'} });
 
-beerStalker.controller('BeerStalkController', ['$scope', '$resource', 'ApiCall', function($scope, $resource, ApiCall) {
-  var lat
-  var lon
-
-  $scope.customSearch = function() { ApiCall.customSearch($scope.cityName).then(function(results){
-    $scope.searchResult = results
-  }); };
-
-  navigator.geolocation.getCurrentPosition(function(position) {
-   lat = position.coords.latitude;
-   lon = position.coords.longitude;
-  });
-
-  $scope.autoSearch = function() {
-    var meetupUrl = 'https://api.meetup.com/2/open_events.json?and_text=true&:text&:lat&:lon&:key&:text_format&:order'
-    
-    var searchLocalResource = $resource(
-      meetupUrl, 
-      {
-        text_format: "text_format=plain",
-        text: "text=free+beer",
-        lat: 'lat=' + lat,
-        lon: 'lon=' + lon,
-        key: 'key=646f252216306e6d712d7c536a3c2565',
-        order: "order=distance",
-        callback: 'JSON_CALLBACK' 
-      },
-        { get: { method: 'JSONP'} });
-
-      searchLocalResource.get().$promise.then(function(response){
+      //$promise syntax is not always necessary (not sure why it's needed here), but the
+      //return value of the searchForEvents.get() is passed into the next function as an argument -
+      //in this case 'response' - you can call it whatever you want.
+      //'return' is needed here because line 116 is also a promise (indicated by the .then), so a return value
+      //is needed. This is then passed into the 'function(results)' bit as an argument (called 'results' but, again,
+      // it could have been called whatever I liked). Then, finally (kind of backwards chronologically), the
+      //'customSearch' function is called in the view with 'customSearch()'
+      //also note that a 'return' on line 100 is needed, because autoSearch needs one?
+      return searchForEvents.get().$promise.then(function(response){
         var filteredResults = [];
 
         for (index = 0; index < response.results.length; index++) {
@@ -105,8 +97,28 @@ beerStalker.controller('BeerStalkController', ['$scope', '$resource', 'ApiCall',
           return
         } else {
           $('.results').show();
-          $scope.searchResult = filteredResults
+          return filteredResults
         }
       });
-    };
+    }
+  }
+});
+
+beerStalker.controller('BeerStalkController', ['$scope', '$resource', 'ApiCall', function($scope, $resource, ApiCall) {
+  var lat
+  var lon
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+   lat = position.coords.latitude;
+   lon = position.coords.longitude;
+  });
+
+  $scope.customSearch = function() { ApiCall.customSearch($scope.cityName).then(function(results){
+    $scope.searchResult = results
+  }); };
+
+
+  $scope.autoSearch = function() { ApiCall.autoSearch(lat, lon).then(function(results){
+    $scope.searchResult = results
+  }); };
 }]);
