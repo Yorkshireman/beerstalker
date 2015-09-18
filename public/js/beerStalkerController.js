fb064b3400cf8f542321e170041c9b37a9315105
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-18 13:25:35 +0100

    loading message now shown. However, error is not handled yet

--- public/js/beerStalkerController.js
+++ public/js/beerStalkerController.js
@@ -1,18 +1,22 @@
 beerStalker.controller('BeerStalkController', ['$scope', '$resource', 'ApiCall', 'GeoLocation', function($scope, $resource, ApiCall, GeoLocation) {
 
   $scope.autoSearch = function() {
+    $scope.loading = true;
     $.when(GeoLocation.getLocation()).then(function(data, textStatus, jqXHR) {
       return [data.coords.longitude, data.coords.latitude];
     }).then(function(location) {
       ApiCall.autoSearch(location[1], location[0]).then(function(results){
         $scope.searchResult = results
+        $scope.loading = false;
       });
     }); 
   };
 
   $scope.customSearch = function() { 
+    $scope.loading = true;
     ApiCall.customSearch($scope.cityName).then(function(results){
       $scope.searchResult = results
+      $scope.loading = false;
     });
   };
 }]);
